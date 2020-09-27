5d042e6526b1e6a199d45c250672016f92a6f2e1
ravelmello <ravelmello3@gmail.com>
2020-09-27 10:44:41 -0300

    Wip medium post

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const userRoute = require('./user/user.routes');
+const authRoute = require('./authRoutes/auth.routes')
 
+router.use(authRoute)
 router.use(userRoute)
+
 
 module.exports = router
--- src/routes/user/user.routes.js
+++ src/routes/user/user.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const UserController = require('../../app/controllers/UserController');
 const userRoute = express.Router();
+const authMiddleware = require('../../app/middlewares/jwt.middleware')
 
 
-userRoute.post('/user', UserController.create)
-userRoute.get('/user', UserController.listAll)
-userRoute.get('/user/:id', UserController.listById)
+userRoute.post('/user', authMiddleware ,UserController.create)
+userRoute.get('/user', authMiddleware,UserController.listAll)
+userRoute.get('/user/:id', authMiddleware, UserController.listById)
 
 module.exports = userRoute
